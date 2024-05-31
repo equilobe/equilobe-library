ee9e54c3e7fb85913647fe75c53974505f891bfe
mihneaMagureanuCSol <mihnea@c-solution.biz>
2024-05-31 12:56:35 +0300

    MihneaMagureanu 31.05

--- Equilobe.Core/Features/Books/Queries/GetBooksQuery.cs
+++ Equilobe.Core/Features/Books/Queries/GetBooksQuery.cs
@@ -1,7 +1,9 @@
 ﻿using Equilobe.Core.Features.Books.DTO;
 using Equilobe.Core.Shared;
+using Equilobe.Core.Shared.Models;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 namespace Equilobe.Core.Features.Books.Queries;
 
@@ -25,4 +27,53 @@ public class GetBooksQueryHandler : IRequestHandler<GetBooksQuery, List<BookDTO>
             .ToListAsync(cancellationToken);
         return books.Select(book => new BookDTO(book)).ToList();
     }
+
+    
+    public async Task<List<BookDTO>> Handle(GetBooksQuery request, CancellationToken cancellationToken,
+        string? bookTitle, BookQualityState? qualityState, bool? isAvailable, SortingState sortId, SortingState sortDate, SortingState sortTitle, int pageSize = 10)
+    {
+        if (pageSize > 50)
+        {
+            pageSize = 50;
+        }
+        if (bookTitle == null)
+        {
+            bookTitle = string.Empty;
+        }
+        var newGuid = new Guid();
+
+        var books = await _dbContext.Books
+            .Include(b => b.Metadata.Author)
+            .Where(b => b.Metadata.Title.ToLower(CultureInfo.InvariantCulture).Contains(bookTitle.ToLower(CultureInfo.InvariantCulture)))
+            .ToListAsync(cancellationToken);
+
+        if (qualityState != null)
+        {
+            books = books.Where(b => b.QualityState == qualityState.Value).ToList();
+        }
+        if (isAvailable != null)
+        {
+            books = books.Where(b => b.IsAvailable == isAvailable.Value).ToList();
+        }
+
+        //cant find a reverse guid function, will treat guid with conditional
+        if (sortId == SortingState.Descending)
+        {
+            books = books.OrderByDescending(b => b.Id)
+            .ThenBy(b => (sortTitle == SortingState.None) ? string.Empty : (sortTitle == SortingState.Ascending) ? b.Metadata.Title : new string(b.Metadata.Title.Reverse().ToArray()))
+            .ThenBy(b => (sortDate == SortingState.None) ? DateTime.Now : (sortDate == SortingState.Ascending) ? b.CreatedAt : DateTime.Now + (DateTime.MaxValue.AddYears(-4000) - b.CreatedAt)).ToList();
+        } else
+        {
+            books = books.OrderBy(b => (sortId == SortingState.None) ? newGuid : b.Id)
+            .ThenBy(b => (sortTitle == SortingState.None) ? string.Empty : (sortTitle == SortingState.Ascending) ? b.Metadata.Title : new string(b.Metadata.Title.Reverse().ToArray()))
+            .ThenBy(b => (sortDate == SortingState.None) ? DateTime.Now : (sortDate == SortingState.Ascending) ? b.CreatedAt : DateTime.Now + (DateTime.MaxValue.AddYears(-4000) - b.CreatedAt)).ToList();
+        }
+
+        books= books.Take(pageSize).ToList();
+
+        
+        return books.Select(book => new BookDTO(book)).ToList();
+    }
+
+
 }
--- Equilobe.Core/Features/Loans/DTO/LoanDTO.cs
+++ Equilobe.Core/Features/Loans/DTO/LoanDTO.cs
@@ -2,15 +2,17 @@
 
 public class LoanDTO
 {
-    public LoanDTO(Loan loan)
+    public LoanDTO(Loan loan, string? bookTitle)
     {
         Id = loan.Id;
         LoanDate = loan.LoanDate;
         DueDate = loan.DueDate;
         ReturnDate = loan.ReturnDate;
+        BookTitle = bookTitle;
     }
     public Guid Id { get; }
     public DateTime LoanDate { get; }
     public DateTime DueDate { get; }
     public DateTime? ReturnDate { get; }
+    public string? BookTitle { get; }
 }
